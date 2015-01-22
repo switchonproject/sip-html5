f2672b0f01d66d1810cde12fc2e4ef3f66e7fdce
Martin Scholl <martin.scholl@cismet.de>
2015-01-22 12:20:53 +0100

    [issue #15] initial table directive with sorting

---
+++ app/scripts/controllers/listViewDirectiveController.js
@@ -0,0 +1,37 @@
+angular.module(
+    'eu.water-switch-on.sip.controllers'
+).controller(
+    'eu.water-switch-on.sip.controllers.listViewDirectiveController',
+    [
+        '$scope',
+        '$filter',
+        'ngTableParams',
+        function ($scope, $filter, NgTableParams) {
+            'use strict';
+
+            var initialSorting;
+
+            $scope.$watch('tableData', function () {
+                $scope.tableParams.reload();
+            });
+
+            initialSorting = {};
+            initialSorting['object.name'] = 'asc';
+
+            $scope.tableParams = new NgTableParams({
+                page: 1,
+                count: 2,
+                sorting: initialSorting
+            }, {
+                counts: [],
+                total: 1,
+                getData: function ($defer, params) {
+                    var ordered;
+
+                    ordered = params.sorting() ? $filter('orderBy')($scope.tableData, params.orderBy()) : $scope.tableData;
+                    $defer.resolve(ordered);
+                }
+            });
+        }
+    ]
+);
