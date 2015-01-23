4f884de1847878caec2fc2eacb272fab36489f42
Pascal Dihé <pascal@cismet.de>
2015-01-23 15:53:09 +0100

    #14 rapid prototype mockup for user story #13

--- app/scripts/controllers/searchFilterRibbonDirectiveController.js
+++ app/scripts/controllers/searchFilterRibbonDirectiveController.js
@@ -6,7 +6,15 @@ angular.module(
             '$scope',
             function ($scope) {
                 'use strict';
-
-            }
-        ]
+                $scope.keywords = ['Biological', 'Chemical', 'Physical', 'Benthic', 'Biomass', 'Descriptive',
+                    'Fish', 'Nekton', 'Pigment', 'Benthic species', 'Fish species', 'Macrophyte species',
+                    'Nekton species', 'Phytoplankton species', 'Zooplankton species',
+                    'Dissolved Gas', 'Dissolved Solids', 'Major', 'Minor', 'Carbon',
+                    'Nitrogen', 'Phosphorus', 'Other organic chemical', 'PCBs', 'Pesticide', 'Area, atmosphere',
+                    'Area, ice', 'flux', 'discharge', 'evaporation', 'precipitation', 'wind', 'ice', 'lake', 'ocean',
+                    'snow', 'stream', 'Optical, water', 'air', 'water', 'suspended solids', 'soil', 'Biological community',
+                    'Biological taxa', 'Indicator Organisms', 'Inorganic', 'Nutrient', 'Organic', 'Oxygen Demand',
+                    'Radiochemical ', 'Stable Isotopes', 'Area', 'Density', 'Energy', 'Length', 'Level', 'Optical',
+                    'Pressure', 'Temperature', 'Velocity', 'Volume', 'Water content'];
+            }]
         );
