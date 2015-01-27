454bb5649a77bbd0f298d51d2d70e16b6cbd91bf
Pascal Dihé <pascal@cismet.de>
2015-01-27 18:31:50 +0100

    #23 search ribbon styling and button

--- app/scripts/controllers/searchFilterRibbonDirectiveController.js
+++ app/scripts/controllers/searchFilterRibbonDirectiveController.js
@@ -6,15 +6,10 @@ angular.module(
         '$scope',
         function ($scope) {
             'use strict';
-            $scope.keywords = ['air', 'Area', 'Atmosphere', 'Benthic', 'Benthic species',
-                'Biological', 'Biological community', 'Biological taxa', 'Biomass', 'Carbon',
-                'Chemical', 'Density', 'Descriptive', 'discharge', 'Dissolved Gas', 'Dissolved Solids',
-                'Energy', 'evaporation', 'Fish', 'Fish species', 'flux', 'Ice', 'Indicator Organisms',
-                'Inorganic', 'lake', 'Length', 'Level', 'Macrophyte species', 'Major', 'Minor', 'Nekton',
-                'Nekton species', 'Nitrogen', 'Nutrient', 'ocean', 'Optical', 'Organic', 'Other organic chemical',
-                'Oxygen Demand', 'PCBs', 'Pesticide', 'Phosphorus', 'Physical', 'Phytoplankton species', 'Pigment',
-                'precipitation', 'Pressure', 'Radiochemical ', 'snow', 'soil', 'Stable Isotopes', 'stream',
-                'suspended solids', 'Temperature', 'Velocity', 'Volume', 'water', 'Water content', 'wind',
-                'Zooplankton species'];
+            $scope.clear = function () {
+                $scope.filterExpressions.universalSearchString = '';
+                $scope.filterExpressions.fromDate = null;
+                $scope.filterExpressions.toDate = null;
+            };
         }]
     );
