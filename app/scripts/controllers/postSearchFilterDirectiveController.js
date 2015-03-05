91717193049b525f389fe600b2adf8dd3b2692f0
Pascal Dihé <pascal@cismet.de>
2015-03-05 16:37:53 +0100

    ui improved

--- app/scripts/controllers/postSearchFilterDirectiveController.js
+++ app/scripts/controllers/postSearchFilterDirectiveController.js
@@ -10,16 +10,18 @@ angular.module(
 
             $scope.$watch('filterTags', function (filterTags, o) {
                 if (filterTags && filterTags !== o && filterTags.length > 0) {
-                    var i, j, filterExpression, tagGroup, param;
+                    var i, j, filterExpression, filterExpressions, tagGroup, param;
                     for (i = 0; i < filterTags.length; i++) {
                         tagGroup = filterTags[i];
                         if (tagGroup && tagGroup.value && tagGroup.value.length > 0) {
                             tagGroup = filterTags[i];
-                            param = '!' + tagGroup;
-                            filterExpression = $scope.postSearchFilterExpressions.getFilterExpressionsByType(param);
-                            if (!filterExpression || filterExpression.length === 0) {
-                                filterExpression = new FilterExpression('!' + tagGroup.key, null, true);
+                            param = '!' + tagGroup.key;
+                            filterExpressions = $scope.postSearchFilterExpressions.getFilterExpressionsByType(param);
+                            if (!filterExpressions || filterExpressions.length === 0) {
+                                filterExpression = new FilterExpression(param, null, true);
                                 $scope.postSearchFilterExpressions.addFilterExpression(filterExpression);
+                            } else {
+                                filterExpression = filterExpressions[0];
                             }
 
                             for (j = 0; j < tagGroup.value.length; j++ ) {
