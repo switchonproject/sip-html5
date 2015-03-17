0398c8114dc8fd4ec1a178f7aff849a82282717d
Martin Scholl <martin.scholl@cismet.de>
2015-03-17 09:44:55 +0100

    [issue #73] proper toolbar result list

--- app/scripts/directives/resultSetDirective.js
+++ app/scripts/directives/resultSetDirective.js
@@ -1,22 +1,39 @@
 angular.module(
     'eu.water-switch-on.sip.directives'
-).directive('resultset',
+).directive('resultList',
     [
-        function () {
+        '$state',
+        function ($state) {
             'use strict';
+            
+            var link, scope;
+            
+            scope= {
+                resultSet: '=',
+                selectedObject: '='
+            };
+            
+            link = function (scope, element, attr, toolbarCtrl) {
+                var toggleVisibility;
+                
+                toggleVisibility = function(state) {
+                    scope.isVisible = (state === 'map');
+                    toolbarCtrl.toggleVisibility('resultList', scope.isVisible);
+                };
+                
+                scope.$on('$stateChangeSuccess', function(event, toState) {
+                    toggleVisibility(toState.name);
+                });
+                
+                toggleVisibility($state.current.name);
+            };
 
             return {
                 restrict: 'E',
-                templateUrl: 'templates/resultset-directive.html',
-                scope: {
-                    resultSet: '=',
-                    selectedObject: '=',
-                    filterExpressions: '=',
-                    performSearch: '&searchFunction',
-                    notificationFunction: '&?'
-                },
-                controller: 'eu.water-switch-on.sip.controllers.resultSetDirectiveController',
-                controllerAs: 'resultSetDirectiveController'
+                require: '^masterToolbar',
+                templateUrl: 'templates/resultlist-directive.html',
+                scope: scope,
+                link: link
             };
         }
     ]);
