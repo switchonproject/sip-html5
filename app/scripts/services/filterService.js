e509b1dc70610df14b76d2a7a9b581b9337f63bb
Pascal Dihé <pascal@cismet.de>
2015-04-07 11:34:28 +0200

    #76 filter parameter to taggroup mapping implemented

--- app/scripts/services/filterService.js
+++ app/scripts/services/filterService.js
@@ -7,36 +7,66 @@ angular.module(
         function (FilterExpression, AppConfig) {
             'use strict';
 
-            var nodeCollection, nodeCollectionCopy, filteredNodes,
+            var nodeCollection, nodeCollectionCopy,
                 setResultSetFunction, filterResultSetFunction, containsTag,
-                filterNodeCollection, totalResources, pattern;
+                filterNodeCollection, totalResources, pattern, parameterToTaggroup,
+                taggroups;
 
             pattern = AppConfig.filterExpressionPattern;
 
+            taggroups = {};
+            taggroups[FilterExpression.FILTER__ACCESS_CONDITION] = 'access conditions';
+            taggroups[FilterExpression.FILTER__KEYWORD_CUAHSI] = 'keywords - cuahsi';
+            taggroups[FilterExpression.FILTER__KEYWORD] = 'keywords - free';
+            taggroups[FilterExpression.FILTER__PROTOCOL] = 'protocol';
+            taggroups[FilterExpression.FILTER__FUNCTION] = 'function';
+            taggroups[FilterExpression.FILTER__RESOURE_TYPE] = 'resource type';
+
             /**
+             * Sets a complete server search result set including node collection 
+             * and objects. The function creates a shallow copy of the results set
+             * to be able to revoke applied filters.
              * 
-             * @param {type} objs
+             * @param {type} resultSet
              * @returns {undefined}
              */
             setResultSetFunction = function (resultSet) {
                 if (resultSet && resultSet.$collection) {
                     nodeCollection = resultSet.$collection;
                     nodeCollectionCopy = nodeCollection.slice();
-                    filteredNodes = [];
                     totalResources = resultSet.$total;
                 } else {
                     nodeCollection = [];
                     nodeCollectionCopy = [];
-                    filteredNodes = [];
                     totalResources = 0;
                 }
             };
 
+            /**
+             * Filters a result set by the FilterExpression. Accepts either a
+             * negated filter expression or a post search filters filter expression
+             * wherby the post search filters filter expression consits of an array 
+             * of negated filter expressions.
+             * 
+             * The filter is directly applied on the shared resultSet$collection
+             * that is injected into the the $scope of this service and other 
+             * directives, e.g. the listViewDirective.
+             * 
+             * @param {FilterExpression} filterExpression FilterExpression used for filtering
+             * @returns {undefined}
+             */
             filterResultSetFunction = function (filterExpression) {
                 var i, filterExpressionString, param, value;
+
+                // reset node collection
+                for (i = 0; i < nodeCollectionCopy.length; ++i) {
+                    if (nodeCollection.indexOf(nodeCollectionCopy[i]) === -1) {
+                        nodeCollection.push(nodeCollectionCopy[i]);
+                    }
+                }
+
+                // re-apply filters
                 if (filterExpression && filterExpression.isValid()) {
-                    // reset!
-                    filteredNodes = [];
                     if (filterExpression.parameter === FilterExpression.FILTER__POST_SEARCH_FILTERS) {
                         for (i = 0; i < filterExpression.value.length; ++i) {
                             // post search filters are an array of negated filter expressions!
@@ -47,7 +77,7 @@ angular.module(
                             if (param && value) {
                                 // filter expression is negated but the tag group name not!
                                 param = param.indexOf('!') === 0 ? param.substr(1) : param;
-                                filterNodeCollection(param, value);
+                                filterNodeCollection(parameterToTaggroup(param), value);
                             }
                         }
                     } else {
@@ -57,59 +87,71 @@ angular.module(
 
                         if (filterExpression.isMultiple()) {
                             for (i = 0; i < filterExpression.value.length; ++i) {
-                                filterNodeCollection(param, value[i]);
+                                filterNodeCollection(parameterToTaggroup(param), value[i]);
                             }
                         } else {
-                            filterNodeCollection(param, value);
+                            filterNodeCollection(parameterToTaggroup(param), value);
                         }
-                    }
-                } else if (filteredNodes.length > 0 || nodeCollectionCopy.length > nodeCollection.length) {
-                    // filter expressions have been removed completely
-                    // but filters were applied. reset them all!
-                    for (i = 0; i < nodeCollectionCopy.length; ++i) {
-                        if (nodeCollection.indexOf(nodeCollectionCopy[i]) === -1) {
-                            nodeCollection.push(nodeCollectionCopy[i]);
-                        }
-                    }
-
-                    if (nodeCollectionCopy.length === nodeCollection.length) {
-                        filteredNodes = [];
-                    } else {
-                        console.error('cannot reset ' + filteredNodes.length + ' post search filters');
                     }
                 }
             };
 
+            /**
+             * Performs a mapping between a filter expression parameter
+             * and a taggroup. In general filter expression parmameters
+             * (e.g. 'access-condition') should directly map to taggroup names.
+             * However, some data models use taggroup names that contain spaces, etc.
+             * (like 'access conditions'), therfore a client-side mapping is necessary-
+             * 
+             * @param {string} param
+             * @returns {string} taggroup mapped to parameter
+             */
+            parameterToTaggroup = function (param) {
+                var taggroup;
+                taggroup = taggroups[param];
+                return taggroup || param;
+            };
+
+            /**
+             * Helper method for filtering a collection of nodes by a tag belonging 
+             * to a specific tag group.
+             * 
+             * @param {string} taggroup
+             * @param {string} tagname
+             * @returns {undefined}
+             */
             filterNodeCollection = function (taggroup, tagname) {
                 var i, j, node;
-                if (nodeCollectionCopy && nodeCollectionCopy.length > 0 &&
-                        nodeCollection && nodeCollection.length > 0) {
-                    for (i = nodeCollectionCopy.length - 1; i >= 0; --i) {
+
+                if (nodeCollectionCopy && nodeCollection && nodeCollectionCopy.length > 0) {
+
+                    for (i = 0; i < nodeCollectionCopy.length; i++) {
                         node = nodeCollectionCopy[i];
                         j = nodeCollection.indexOf(node);
                         // omit node properties, start directly with object
                         if (containsTag(null, node.object, taggroup, tagname)) {
                             // filter can be applied to node!
                             // remove the node if it is not already removed
+                            //console.log('tag "' + tagname + '" of taggroup "' + taggroup + ' filtered in nodeCollection[' + nodeCollection.length + '] (total nodes:' + nodeCollectionCopy.length + ')');
                             if (j !== -1) {
-                                filteredNodes.push(node);
+                                //console.log('removing tag "' + tagname + '" of taggroup "' + taggroup + '" from nodeCollection[' + nodeCollection.length + '] (total nodes:' + nodeCollectionCopy.length + ') at position ' + j);
                                 nodeCollection.splice(j, 1);
-                            }
-                        } else {
-                            // the node is not in the list
-                            if (j === -1) {
-                                // and it is not filtered!
-                                if (filteredNodes.indexOf(node) === -1) {
-                                    // the user just removed the post search filter from USB again
-                                    // re-add it to the displyed results 
-                                    nodeCollection.push(j, nodeCollectionCopy[i]);
-                                }
                             }
                         }
                     }
                 }
             };
 
+            /**
+             * Helper method for tag-based filtering that checks whether an
+             * (cids)object is associated with a tag of a specific tag group.
+             * 
+             * @param {object} parentObject
+             * @param {object} childObject
+             * @param {string} taggroup
+             * @param {string} tagname
+             * @returns {Boolean} true if the child object contains the tag
+             */
             containsTag = function (parentObject, childObject, taggroup, tagname) {
                 var propertyName, propertyValue;
                 if (parentObject && childObject && childObject.hasOwnProperty('taggroup')) {
@@ -138,7 +180,7 @@ angular.module(
             return {
                 setResultSet: setResultSetFunction,
                 getTotalResourcesNumber: function () { return totalResources; },
-                getFilteredResourcesNumber: function () { return filteredNodes.length; },
+                getFilteredResourcesNumber: function () { return nodeCollectionCopy.length - nodeCollection.length; },
                 getLoadedResourcesNumber: function () { return nodeCollectionCopy.length; },
                 isCompleteResult: function () { return nodeCollectionCopy && nodeCollectionCopy.length > 0 && nodeCollectionCopy.length ===  totalResources; },
                 filterResultSet: filterResultSetFunction
