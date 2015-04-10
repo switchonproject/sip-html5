df09f660f4e60fa82f9c5affd088f7e69cb603fe
Pascal Dihé <pascal@cismet.de>
2015-04-10 17:11:39 +0200

    update of public beta release #66 with latest bugfixes

--- app/scripts/factories/AppConfigFactory.js
+++ app/scripts/factories/AppConfigFactory.js
@@ -27,9 +27,9 @@ angular.module(
         appConfig.searchService.username = 'admin@SWITCHON';
         appConfig.searchService.password = 'cismet';
         //appConfig.searchService.host = 'http://localhost:8890';
-        appConfig.searchService.defautLimit = 10;
+        appConfig.searchService.defautLimit = 20;
         appConfig.searchService.maxLimit = 50;
-        appConfig.searchService.host = 'http://switchon.cismet.de/legacy-rest1';
+        appConfig.searchService.host = 'http://tl-ap001.xtr.deltares.nl/switchon_server_rest';
 
         appConfig.mapView = {};
         appConfig.mapView.backgroundLayer = 'http://{s}.opentopomap.org/{z}/{x}/{y}.png';
