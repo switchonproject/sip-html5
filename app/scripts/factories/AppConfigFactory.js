611dd1930871b053e397fbcbf3d690e92706f480
Pascal Dihé <pascal@cismet.de>
2016-03-09 09:12:58 +0100

    #124 service url changed

--- app/scripts/factories/AppConfigFactory.js
+++ app/scripts/factories/AppConfigFactory.js
@@ -29,8 +29,8 @@ angular.module(
                 appConfig.searchService.defautLimit = 10;
                 appConfig.searchService.maxLimit = 50;
                 //appConfig.searchService.host = 'http://localhost:8890';
-                appConfig.searchService.host = 'http://switchon.cismet.de/legacy-rest1';
-                //appConfig.searchService.host = 'http://tl-243.xtr.deltares.nl/switchon_server_rest';
+                //appConfig.searchService.host = 'http://switchon.cismet.de/legacy-rest1';
+                appConfig.searchService.host = 'http://tl-243.xtr.deltares.nl/switchon_server_rest';
 
                 appConfig.mapView = {};
                 appConfig.mapView.backgroundLayer = 'http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png';
