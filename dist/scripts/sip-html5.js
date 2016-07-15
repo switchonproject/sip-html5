381498093fe7ada132c40315fe2739d6e4ad81c3
p-a-s-c-a-l <pascal@cismet.de>
2016-07-15 09:15:19 +0200

    #128 upgrade dependencies

--- dist/scripts/sip-html5.js
+++ dist/scripts/sip-html5.js
@@ -2447,8 +2447,8 @@ angular.module(
                 appConfig.searchService.password = 'cismet';
                 appConfig.searchService.defautLimit = 10;
                 appConfig.searchService.maxLimit = 50;
-                appConfig.searchService.host = 'http://localhost:8890';
-                //appConfig.searchService.host = 'http://switchon.cismet.de/legacy-rest1';
+                //appConfig.searchService.host = 'http://localhost:8890';
+                appConfig.searchService.host = 'http://switchon.cismet.de/legacy-rest1';
                 //appConfig.searchService.host = 'http://tl-243.xtr.deltares.nl/switchon_server_rest';
 
                 appConfig.mapView = {};
